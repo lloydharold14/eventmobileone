909e7bcbbc369423ec858fb06098ed8fc30e8cb7
lloydharold14 <lloydharold14@gmail.com>
2025-08-21 09:17:58 -0400

    feat: remove preview

--- composeApp/src/commonMain/kotlin/com/eventsmobileone/App.kt
+++ composeApp/src/commonMain/kotlin/com/eventsmobileone/App.kt
@@ -1,14 +1,12 @@
 package com.eventsmobileone
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 /**
  * Main App Entry Point
  * This is the main composable that gets called from platform-specific launchers
  */
 @Composable
-@Preview
 fun App() {
     AppRoot()
 }
